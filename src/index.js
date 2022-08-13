1b98131c7513ec414d5ceaa5b89ebf8688578c7d
mohsen <mohsen.bahrami7690@gmail.com>
2022-08-13 16:29:10 +0430

    complate main

--- src/index.js
+++ src/index.js
@@ -22,9 +22,8 @@ function pointNumber(valueNumber) {
 //////////point number-end//////////
 
 
-//////////top offer//////////
-let topOfferIndex = [1, 5, 15, 3, 6, 10, 11]
-function MakeCardsProduct(productList, targetIdDoc) {
+//////////make card-end//////////
+function MakeCardsProduct(productList, targetIdDoc, targetChild = null) {
     let targetElemment = document.querySelector(`#${targetIdDoc}`)
     productList.forEach((productIndex) => {
         let productData = products[productIndex];
@@ -33,11 +32,11 @@ function MakeCardsProduct(productList, targetIdDoc) {
         productCard.innerHTML = `
         <div class="card-header">
             <div class="nav nav-tabs card-header-tabs" id="" role="tablist">
-                <button type="button" class="nav-link active" data-toggle="tab" data-target="#t-o-${productData.proId}-pro"
+                <button type="button" class="nav-link active" data-toggle="tab" data-target="#${targetIdDoc}-${productData.proId}-pro"
                     aria-controls="product" aria-selected="true">
                     محصول
                 </button>
-                <button type="button" class="nav-link" data-toggle="tab" data-target="#t-o-${productData.proId}-com"
+                <button type="button" class="nav-link" data-toggle="tab" data-target="#${targetIdDoc}-${productData.proId}-com"
                     aria-controls="comments" aria-selected="true">
                     نظرات
                 </button>
@@ -45,7 +44,7 @@ function MakeCardsProduct(productList, targetIdDoc) {
         </div>
         <div class="card-body mt-2">
             <div class="tab-content">
-                <div class="nav-pane position-relative fade show active" id="t-o-${productData.proId}-pro" role="tabpanel"
+                <div class="nav-pane position-relative fade show active" id="${targetIdDoc}-${productData.proId}-pro" role="tabpanel"
                     aria-labelledby="product-tab">
                     <img class="card-img" src="${productData.imgURL}" alt="">
                     <div class="card-title">${productData.title}</div>
@@ -60,7 +59,7 @@ function MakeCardsProduct(productList, targetIdDoc) {
                         <i class="fa-solid fa-cart-plus"></i>
                     </button>
                 </div>
-                <div class="px-2 nav-pane fade" id="t-o-${productData.proId}-com" role="tabpanel"
+                <div class="px-2 nav-pane fade" id="${targetIdDoc}-${productData.proId}-com" role="tabpanel"
                     aria-labelledby="comments-tab">
                     <div class="media mt-2">
                         <i class="fa-solid fa-circle-user d-inline" style="font-size: x-large;"></i>
@@ -91,11 +90,26 @@ function MakeCardsProduct(productList, targetIdDoc) {
             </div>
         </div>    
         `
-        targetElemment.querySelector('.scroll-part').appendChild(productCard)
+        if(targetChild){
+            targetElemment.querySelector(`.${targetChild}`).appendChild(productCard)
+        } else if(!targetChild){
+            targetElemment.appendChild(productCard)
+        }
     })
 }
-MakeCardsProduct(topOfferIndex, "top-offer");
+//////////make card-end//////////
+
+
+//////////top offer//////////
+let topOfferIndex = [1, 5, 15, 3, 6, 10, 11]
+MakeCardsProduct(topOfferIndex, "top-offer", "scroll-part");
 //////////top offer-end//////////
+
+
+//////////all products//////////
+let allProducts = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+MakeCardsProduct(allProducts, "all-product");
+//////////all products-end//////////
 
 
 //////////not offer//////////
@@ -110,8 +124,8 @@ Array.from(document.querySelectorAll('.point-number')).forEach((text) => {
 //////////not offer-end//////////
 
 
-//////////input point number//////////
+//////////point number//////////
 Array.from(document.querySelectorAll(".point-number")).forEach((numTag) => {
     numTag.innerHTML = pointNumber(numTag.innerHTML)
 })
-//////////input point number-end//////////
+//////////point number-end//////////
